a64c2bf1c161044ab4fc2d2224e77206c1e08799
Pankaj jangir <68711037+LazyScientist@users.noreply.github.com>
2022-03-28 11:20:14 +0530

    Update Demo_App.py

--- Demo_App.py
+++ Demo_App.py
@@ -20,24 +20,38 @@ Window.softinput_mode = "below_target"
 if platform.system() == "Windows":
     os.environ["KIVY_GL_BACKEND"] = "angle_sdl2"
 else:
-    if not check_permission(Permission.RECORD_AUDIO):
-        request_permission(Permission.RECORD_AUDIO)
-    if not check_permission(Permission.CAMERA):
-        request_permission(Permission.CAMERA)
-    else:
-        print('Permission OK')
-    Window.clear()
-    Window.borderless = True
-    Window.fullscreen = True
-    Window.maximize() 
+    try:
+        if not check_permission(Permission.RECORD_AUDIO):
+            request_permission(Permission.RECORD_AUDIO)
+        if not check_permission(Permission.CAMERA):
+            request_permission(Permission.CAMERA)
+        else:
+            print('Permission OK')
+        Window.clear()
+        Window.borderless = True
+        Window.fullscreen = True
+        Window.maximize()
+    except:
+        pass
 
 
 class Demo_App(MDApp):  # NOQA: N801
     def __init__(self, **kwargs):
         super(Demo_App, self).__init__(**kwargs)
         Window.soft_input_mode = "below_target"
+        # change your app loading image in buildozer.spec file
+        # (str) Presplash of the application
+        # presplash.filename = %(source.dir)s/assets/icons/splash_screen.png
+
+        # change the icon of your app in buildozer.spec file and then at here, change path for self.icon
+        # (str) Icon of the application
+        # icon.filename = %(source.dir)s/assets/icons/my_icon.png
         self.icon= os.path.join('assets','icons','my_icon.png')
-        self.title = "My_Demo_App"
+        
+        # change the name of your app at buildozer.spec file and here also
+        # (str) Title of your application
+        # title = Demo_App
+        self.title = "Demo_App"
 
         self.theme_cls.primary_palette = "DeepOrange"
         self.theme_cls.primary_hue = "500"
